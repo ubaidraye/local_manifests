d7a8431a4450706a52c52ee7a193fa92ea974393
ubaidraye <ubaidraye706@gmail.com>
2026-02-04 23:16:36 +0000

    Add derpfest build script

---
+++ derpfest.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+rm -rf prebuilts/clang/host/linux-x86
+rm -rf kernel/motorola/sm8475 kernel/motorola/sm8475-devicetrees kernel/motorola/sm8475-modules
+rm -rf hardware/motorola device/motorola/eqs device/motorola/sm8475-common vendor/motorola/eqs vendor/motorola/sm8475-common
+rm -rf .repo/local_manifests
+
+git clone https://github.com/ubaidraye/local_manifests .repo/local_manifests
+repo init -u https://github.com/DerpFest-AOSP/android_manifest.git -b 16.2 --git-lfs --no-clone-bundle
+/opt/crave/resync.sh || repo sync
+
+export BUILD_USERNAME=android-build
+export DISABLE_STUB_VALIDATION=true
+
+. build/envsetup.sh
+lunch lineage_eqs-bp4a-userdebug
+make installclean
+mka derp
