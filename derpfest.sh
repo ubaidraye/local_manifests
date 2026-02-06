ee17006b7814da6e74e703f33c7694a46b775414
ubaidraye <ubaidraye706@gmail.com>
2026-02-06 15:25:41 +0530

    script: apply @KernelSU-Next patch

--- derpfest.sh
+++ derpfest.sh
@@ -8,7 +8,15 @@ rm -rf .repo/local_manifests
 
 repo init -u https://github.com/DerpFest-AOSP/android_manifest.git -b 16.2 --git-lfs --no-clone-bundle --depth=1
 git clone https://github.com/ubaidraye/local_manifests .repo/local_manifests
+
 /opt/crave/resync.sh || repo sync
+
+# apply kernelsu-next
+cd kernel/motorola/sm8475
+curl -LSs "https://raw.githubusercontent.com/KernelSU-Next/KernelSU-Next/next/kernel/setup.sh" | bash -
+
+#return to roms root
+cd ../../..
 
 export BUILD_USERNAME=RayeUB
 export BUILD_HOSTNAME=crave
