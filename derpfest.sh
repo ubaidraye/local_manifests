0b00b55d9b2f2b5c38c4435ad9449b467e8bf682
ubaidraye <ubaidraye706@gmail.com>
2026-02-06 19:40:39 +0530

    manifest: do not sync vendor from manifest due to git lfs error script: sync vendor and extract new blobs

--- derpfest.sh
+++ derpfest.sh
@@ -5,6 +5,7 @@ rm -rf device/motorola
 rm -rf kernel/motorola
 rm -rf vendor/motorola
 rm -rf .repo/local_manifests
+rm -rf *.zip
 
 repo init -u https://github.com/DerpFest-AOSP/android_manifest.git -b 16.2 --git-lfs --no-clone-bundle --depth=1
 git clone https://github.com/ubaidraye/local_manifests .repo/local_manifests
@@ -18,6 +19,19 @@ curl -LSs "https://raw.githubusercontent.com/KernelSU-Next/KernelSU-Next/next/ke
 #return to roms root
 cd ../../..
 
+# clone vendor
+git clone https://github.com/themuppets/proprietary_vendor_motorola_eqs vendor/motorola/eqs --depth=1
+
+git clone https://github.com/ubaidraye/proprietary_vendor_motorola_sm8475-common -b lineage-23.2 vendor/motorola/sm8475-common --depth=1
+
+# get A15 Vendor
+wget https://mirrors.lolinet.com/firmware/lenomola/2022/eqs/official/RETAIL/EQS_RETAIL_15_V1SQS35H.58-10-8-1_subsidy_DEFAULT_regulatory_DEFAULT_cid50_CFC.xml.zip
+
+unzip EQS_RETAIL_15_V1SQS35H.58-10-8-1*.zip -d device/motorola/eqs/A15-vendor
+
+# extract new vendor
+cd device/motorola/eqs; ./setup-makefiles.py; ./extract-files.py A15-vendor
+cd ../../..
 export BUILD_USERNAME=RayeUB
 export BUILD_HOSTNAME=crave
 export DISABLE_STUB_VALIDATION=true
